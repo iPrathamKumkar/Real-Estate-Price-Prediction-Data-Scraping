8f736df73cb9c45eb883218fe24c5750e3959431
Prathamesh Kumkar <35702012+iPrathamKumkar@users.noreply.github.com>
2018-06-20 03:03:50 +0530

    Made compatible with Python 3

--- Data_Extraction.py
+++ Data_Extraction.py
@@ -4,25 +4,25 @@ from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
 
-chrome = r"C:\Users\Ishant\Downloads\chromedriver.exe" #chromedriver
-driver = webdriver.Chrome(chrome) #initialise chrome webdriver
+chrome = r"C:\Users\Ishant\Downloads\chromedriver.exe"  # chromedriver
+driver = webdriver.Chrome(chrome)  # initialise chrome webdriver
 url_extension = 'https://www.99acres.com/'
-urls_page = ['https://www.99acres.com/property-in-santacruz-west-mumbai-south-west-ffid-page-'+
-             str(i) for i in range(1, 10)] #pagenation links
+urls_page = ['https://www.99acres.com/property-in-santacruz-west-mumbai-south-west-ffid-page-' +
+             str(i) for i in range(1, 10)]  # pagenation links
 
-with open('C:\Users\Ishant\Desktop\\' + 'Santacruz(West).csv', 'a') as f: #creating csv file to store extracted data
+with open(r'C:\Users\Ishant\Desktop\\' + 'Santacruz(West).csv', 'a') as f:  # creating csv file to store extracted data
     writer = csv.writer(f)
     rows = zip(['SuperBuiltUp Area'], ['Area'], ['Carpet Area'], ['Configuration'], ['Price'], ['Price per sq.Ft'], ['Age'],
                ['Floor Number'], ['Address'], ['Parking'], ['Furnishing'], ['Location'], '\n')
     for row in rows:
-        print row
+        print(row)
         writer.writerow(row)
         f.close
 for x in urls_page:
     try:
-        driver.get(x) #connecting to the required website
+        driver.get(x)  # connecting to the required website
         try:
-            while True: #used to simulate scrolling action of a webpage on selenium browser
+            while True:  # used to simulate scrolling action of a webpage on selenium browser
                 last_height = driver.execute_script("return document.body.scrollHeight")
                 # Scroll down to bottom
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -36,21 +36,21 @@ for x in urls_page:
                     break
                 last_height = new_height
         except:
-            print ''
+            print('')
 
         page = driver.page_source
-        soup = BeautifulSoup(page,'html.parser')
-        ab = soup.findAll('div', {"class": "wrapttl"}) #get all span tags which contain 'a' tags
+        soup = BeautifulSoup(page, 'html.parser')
+        ab = soup.findAll('div', {"class": "wrapttl"})  # get all span tags which contain 'a' tags
 
-        for span in ab: #go through all links found on the page
+        for span in ab:  # go through all links found on the page
             try:
-                links = span.find_all('a') #extract the required links only
+                links = span.find_all('a')  # extract the required links only
                 for i in links:
                     driver.get(url_extension+i['href'])
                     pg = driver.page_source
                     soup = BeautifulSoup(pg, 'html.parser')
 
-                    area = soup.find('span',{'id':'builtupArea_span'})
+                    area = soup.find('span', {'id': 'builtupArea_span'})
                     if area is None:
                         area = '-'
                     else:
@@ -86,7 +86,7 @@ for x in urls_page:
                         configuration = '-'
                     else:
                         configuration = configuration
-                    price = soup.find('span',{'class':'pdPropValue'})
+                    price = soup.find('span', {'class': 'pdPropValue'})
                     if price is None:
                         price = '-'
                     else:
@@ -95,9 +95,9 @@ for x in urls_page:
                     if price_per_sq_feet is None:
                         price_per_sq_feet = '-'
                     else:
-                         price_per_sq_feet = price_per_sq_feet.text
-                         price_per_sq_feet = re.sub('View Price Details','',price_per_sq_feet)
-                         price_per_sq_feet = re.sub('@','',price_per_sq_feet)
+                        price_per_sq_feet = price_per_sq_feet.text
+                        price_per_sq_feet = re.sub('View Price Details', '', price_per_sq_feet)
+                        price_per_sq_feet = re.sub('@', '', price_per_sq_feet)
                     age = driver.find_element_by_xpath("""//*[@id="agePossessionLbl"]""")
                     if age is None:
                         age = '-'
@@ -120,7 +120,7 @@ for x in urls_page:
                     else:
                         parking = parking.text
                         if parking.find('Covered'):
-                            parking = re.sub('Covered','',parking)
+                            parking = re.sub('Covered', '', parking)
                         if parking.find('Open'):
                             parking = re.sub('Open', '', parking)
 
@@ -137,28 +137,23 @@ for x in urls_page:
                         address2 = address2.text
                     address = address1+address2
 
-                    print superbuiltuparea
-                    print carpetarea
-                    with open('C:\Users\Ishant\Desktop\\'+'Santacruz(West).csv','a') as f:
+                    print(superbuiltuparea)
+                    print(carpetarea)
+                    with open(r'C:\Users\Ishant\Desktop\\'+'Santacruz(West).csv', 'a') as f:
                         writer = csv.writer(f)
-                        rows = zip([superbuiltuparea], [area],[carpetarea],[configuration], [price],[price_per_sq_feet],[age],[floorno],[address],[parking],[furnishing] ,['Santacruz West'],'\n') #zip takes multiple iterables and returns an iterator of tuples
+                        rows = zip([superbuiltuparea], [area], [carpetarea], [configuration], [price], [price_per_sq_feet], [age], [floorno], [address], [parking], [furnishing], ['Santacruz West'], '\n')  # zip takes multiple iterables and returns an iterator of tuples
                         for row in rows:
-                            print row
+                            print(row)
                             writer.writerow(row)
                             f.close
             except Exception as e:
-                with open('C:\Users\Ishant\Desktop\\' + 'log_Santacruz(West).csv', 'a') as f1:
-                         f1.write(str(url_extension+i['href'])+'\n')
-                         f1.close()
+                with open(r'C:\Users\Ishant\Desktop\\' + 'log_Santacruz(West).csv', 'a') as f1:
+                        f1.write(str(url_extension+i['href'])+'\n')
+                        f1.close()
     except Exception as e1:
-        with open('C:\Users\Ishant\Desktop\\' + 'log_Santacruz(West)_pagelinks.csv', 'a') as f1:
+        with open(r'C:\Users\Ishant\Desktop\\' + 'log_Santacruz(West)_pagelinks.csv', 'a') as f1:
             f1.write(str(url_extension + i['href']) + '\n')
             f1.close()
 
 
 driver.close()
-
-
-
-
-
